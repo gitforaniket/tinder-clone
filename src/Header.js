8d5952a5189f6b1e1b3df443d98a89ba7378443d
Aniket Kumar <imanik8@hotmail.com>
2021-05-10 01:20:51 +0530

    swipebuttons-done

--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,28 @@
-import React from 'react'
+import React from "react";
 import "./Header.css";
-import PersonIcon from '@material-ui/icons/Person';
-import IconButton from '@material-ui/core/IconButton';
-import ForumIcon from '@material-ui/icons/Forum'
-import { Icon } from '@material-ui/core';
+import PersonIcon from "@material-ui/icons/Person";
+import IconButton from "@material-ui/core/IconButton";
+import ForumIcon from '@material-ui/icons/Forum';
 
 function Header() {
-    return (
-        <div className="header">
+  return (
+    <div className="header">
+      <IconButton>
+        <PersonIcon fontSize="large" className="header__icon" />
+      </IconButton>
 
-            <IconButton>
-                <PersonIcon fontSize="large" className="header_icon" />
-            </IconButton>
-
-            <img className="header__logo" src="https://cdn.worldvectorlogo.com/logos/tinder-2.svg" alt="" />
-
-            <IconButton>
-                <ForumIcon fontSize="large" className="header_icon"/>
-            </IconButton>
-
-        </div>
-    )
+      <img
+        className="header__logo"
+        src="https://cdn.worldvectorlogo.com/logos/tinder-2.svg"
+        alt=""
+      />
+      <IconButton>
+        <ForumIcon fontSize="large" className="header__icon" />
+      </IconButton>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
+
+
