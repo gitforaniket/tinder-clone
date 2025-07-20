c200f7c3ef731dbb1e2d31d27a1fb9bad7e9b474
Aniket <heyaniket@hotmail.com>
2025-07-20 19:45:49 +0530

    fix: backend compatibility (Express 4, Mongoose async/await, remove deprecated options)

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 //DB Config
 mongoose.connect(connection_url, {
     useNewUrlParser: true,
-    useCreateIndex: true,
     useUnifiedTopology: true,
 })
 
@@ -32,27 +31,24 @@ app.get('/', (req, res) => {
     res.status(200).send('Hello World!!')
 });
 
-app.post('/tinder/Cards', (req, res) => {
+app.post('/tinder/Cards', async (req, res) => {
     const dbCard = req.body;
-    Cards.create(dbCard, (err, data) => {
-        if(err){
-            res.status(500).send(err)
-        } else {
-            res.status(201).send(data)
-        }
-    })
-})
+    try {
+        const data = await Cards.create(dbCard);
+        res.status(201).send(data);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 
-app.get('/tinder/Cards', (req, res) => {
-    const dbCard = req.body;
-    Cards.find((err, data) => {
-        if(err){
-            res.status(500).send(err)
-        } else {
-            res.status(200).send(data)
-        }
-    })
-})
+app.get('/tinder/Cards', async (req, res) => {
+    try {
+        const data = await Cards.find();
+        res.status(200).send(data);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 
 // Fallback: serve React index.html for any unknown route
 app.get('*', (req, res) => {
